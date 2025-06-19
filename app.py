b21210657e92834479327716134d7a944a7b4cf2
arjungiridas <arjungiridas123456@gmail.com>
2025-06-19 14:54:46 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-# test trigger
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello():
+def home():
     return 'Hello from Flask!'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
+
